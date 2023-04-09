7392b4236de86086bd5c2f5762ceb674e22bba61
SnehalVJ26 <129160574+SnehalVJ26@users.noreply.github.com>
2023-04-10 01:10:05 +0530

    change HTTPClinet to protocol from abstract class

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,18 +8,20 @@
 import XCTest
 
 class RemoteFeedLoader {
+    var client: HTTPClient
+    init(client: HTTPClient) {
+        self.client = client
+    }
     func load() {
-        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: URL(string: "https://a-url.com")!)
     }
 }
-class HTTPClient {
-    static var shared = HTTPClient()
-    func get(from url:URL) {
-    }
+protocol HTTPClient {
+    func get(from url:URL)
 }
 
 class HTTPClientSpy: HTTPClient {
-    override func get(from url:URL) {
+    func get(from url:URL) {
         requestURL = url
     }
     var requestURL: URL?
@@ -36,16 +38,14 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
 
     func test_init_doesNotRequestDataFromURL() {
-        _ = RemoteFeedLoader()
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
+        _ = RemoteFeedLoader(client: client)
         XCTAssertNil(client.requestURL)
     }
     
     func test_load_dataFromURL() {
-        let sut = RemoteFeedLoader()
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
+        let sut = RemoteFeedLoader(client: client)
         sut.load()
         XCTAssertNotNil(client.requestURL)
     }
