1e7fa56fdfab751a0d36011b6c1232cf7eb3b223
SnehalVJ26 <129160574+SnehalVJ26@users.noreply.github.com>
2023-04-10 00:59:08 +0530

    Remove Singleton to move from test logic to test type

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,12 +9,19 @@ import XCTest
 
 class RemoteFeedLoader {
     func load() {
-        HTTPClient.shared.requestURL = URL(string: "https://a-url.com")
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
     }
 }
 class HTTPClient {
-    static let shared = HTTPClient()
-    private init() {}
+    static var shared = HTTPClient()
+    func get(from url:URL) {
+    }
+}
+
+class HTTPClientSpy: HTTPClient {
+    override func get(from url:URL) {
+        requestURL = url
+    }
     var requestURL: URL?
 }
 
@@ -30,13 +37,15 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         _ = RemoteFeedLoader()
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         XCTAssertNil(client.requestURL)
     }
     
     func test_load_dataFromURL() {
         let sut = RemoteFeedLoader()
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         sut.load()
         XCTAssertNotNil(client.requestURL)
     }
