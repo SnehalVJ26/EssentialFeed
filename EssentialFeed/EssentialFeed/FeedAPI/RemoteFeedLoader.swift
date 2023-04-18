f4787af119a0cb15f4b1146197071573f5134305
SnehalJadhav <sjadhav@carnival.com>
2023-04-18 14:49:46 +0530

    added error completion block in HTTPClientSpy and RemoteFeedLoader

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -9,17 +9,22 @@ import Foundation
 
 
 public protocol HTTPClient {
-    func get(from url:URL)
+    func get(from url:URL, completion: @escaping (Error) -> Void)
 }
 
 public final class RemoteFeedLoader {
     private var url: URL
     private var client: HTTPClient
+    public enum Error: Swift.Error {
+        case connectivity
+    }
     public init(client: HTTPClient, url: URL = URL(string: "https://a-url.com")!) {
         self.client = client
         self.url = url
     }
-    public func load() {
-        client.get(from: url)
+    public func load(completion: @escaping (Error) -> Void = { _ in }) {
+        client.get(from: url) { error in
+            completion(.connectivity)
+        }
     }
 }
